1217a87a60d8913a045c3388cffe1dbc19573133
Derek Ellis <derekellis170@gmail.com>
2024-02-25 20:05:29 -0500

    Switch to chunked downloading

--- src/commonMain/kotlin/ca/derekellis/porter/repository/Downloader.kt
+++ src/commonMain/kotlin/ca/derekellis/porter/repository/Downloader.kt
@@ -2,10 +2,13 @@ package ca.derekellis.porter.repository
 
 import io.ktor.client.HttpClient
 import io.ktor.client.plugins.onDownload
-import io.ktor.client.request.get
+import io.ktor.client.request.prepareGet
 import io.ktor.client.statement.bodyAsChannel
+import io.ktor.client.utils.DEFAULT_HTTP_BUFFER_SIZE
 import io.ktor.http.Url
 import io.ktor.utils.io.ByteReadChannel
+import io.ktor.utils.io.core.isEmpty
+import io.ktor.utils.io.core.readBytes
 import okio.FileSystem
 import okio.Path
 import okio.buffer
@@ -20,27 +23,28 @@ class Downloader(
    * @param progress Callback that reports download progress in the range of `0..100`.
    */
   suspend fun get(url: Url, destination: Path, progress: (Int) -> Unit) {
-    val response = httpClient.get(url) {
+    val request = httpClient.prepareGet(url) {
       onDownload { bytesSentTotal, contentLength ->
         val percent = ((bytesSentTotal.toDouble() / contentLength) * 100).toInt()
         progress(percent)
       }
     }
-    val incomingBytes = response.bodyAsChannel()
-    fileSystem.copy(incomingBytes, destination)
+
+    request.execute { httpResponse ->
+      val channel = httpResponse.bodyAsChannel()
+      fileSystem.copy(channel, destination)
+    }
   }
 
   private suspend fun FileSystem.copy(source: ByteReadChannel, target: Path) {
-    val bufferSize = 1024
-    val byteBuffer = ByteArray(bufferSize)
-
     sink(target).buffer().use { sink ->
-      var offset = 0
-      do {
-        val read = source.readAvailable(byteBuffer, offset, bufferSize)
-        sink.write(byteBuffer)
-        offset += read
-      } while (read > 0)
+      while (!source.isClosedForRead) {
+        val packet = source.readRemaining(DEFAULT_HTTP_BUFFER_SIZE.toLong())
+        while (!packet.isEmpty) {
+          val bytes = packet.readBytes()
+          sink.write(bytes)
+        }
+      }
     }
   }
 }
